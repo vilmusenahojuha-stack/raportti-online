0b5d5acf874e0c8d3ff1b4f2cf5dc32342007f05
vilmusenahojuha-stack <vilmusenahojuha@gmail.com>
2026-03-10 14:31:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIG
 const ALLOWED_EMAIL = "juha.vilmusenaho2026@gmail.com";
 const CLIENT_ID = "767469865393-5m24jc369g65fh5d51mcu1moocjd27r9.apps.googleusercontent.com";
-const SHEETS_URL = "PASTE_YOUR_APPS_SCRIPT_WEBAPP_EXEC_URL_HERE";
+const SHEETS_URL = "https://script.google.com/macros/s/AKfycby-qGhTBMk2cASKCAkG_Sv1SHxgBXHjCFDAdctu2qyRQ1Q-wMR-EIVud7evkZXdgHsdGg/exec";
 // ===============================
 
 const LS_EMAIL = "kr_email_v4";
