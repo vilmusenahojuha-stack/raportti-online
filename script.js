21550a5c0403000a73dc69d4d070b327586aaa7d
vilmusenahojuha-stack <vilmusenahojuha@gmail.com>
2026-03-11 09:47:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIG
 const ALLOWED_EMAIL = "juha.vilmusenaho2026@gmail.com";
 const CLIENT_ID = "767469865393-5m24jc369g65fh5d51mcu1moocjd27r9.apps.googleusercontent.com";
-const SHEETS_URL = "https://script.google.com/macros/s/AKfycbwDLJBvzDWlCasnazJ_FnsUJKem3hNC80qcrgY2mgqgk3E57ovq8AM99UDZi3SmjScf/exec";
+const SHEETS_URL = "https://script.google.com/macros/s/AKfycbzGsq5vgnIdxN-fMKVYFcBHXWIa_lnGgwQNKbdC5IVJKSyabUk3e5ksLJAARl6M1RxJ/exec";
 // ===============================
 
 const LS_TOKEN = "kr_idtoken_v3";
